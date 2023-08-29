b13ba8c02640f23bb9b18dbd7173f5e504bb9370
hendr1km <hendrikm@outlook.de>
2023-08-29 15:50:27 +0200

    section_na added in drop_summary function

--- R/drop_prepare.R
+++ R/drop_prepare.R
@@ -4,70 +4,70 @@
 #' @return A list containing a dataframe or tibble with the following for each row of your dataset: A logical whether a dropout has been detected and, if so, the column name where it occurs and the index of the column. As well as the value of the col_index
 
 
-drop_prepare <- function(data, last_col) {
- # Check if there is the optional operator last col, otherwise use data
- if (missing(last_col)) {
-  data
- } else {
-  if (is.character(last_col)) {
-   col_index <- which(names(data) == last_col)
-  } else if (is.numeric(last_col)) {
-   col_index <- last_col
-  } else {
-   stop("Column must be specified by name or index.")
-  }
+  drop_prepare <- function(data, last_col) {
+    # Check if there is the optional operator last col, otherwise use data
+    if (missing(last_col)) {
+      data
+    } else {
+      if (is.character(last_col)) {
+        col_index <- which(names(data) == last_col)
+      } else if (is.numeric(last_col)) {
+        col_index <- last_col
+      } else {
+        stop("Column must be specified by name or index.")
+      }
 
-  if (length(col_index) != 1) {
-   stop("Column not found or ambiguous.")
-  }
+      if (length(col_index) != 1) {
+        stop("Column not found or ambiguous.")
+      }
 
-  data <- data[, 1:col_index]
+      data <- data[, 1:col_index]
 
- }
-
- data_subset <-
-  output <- apply(data, 1, function(row) {
-   dropout_col <- character(1)
-   for (i in 1:(ncol(data))) {
-    if (all(is.na(row[i:ncol(data)]))) {
-     dropout_col <- colnames(data)[i]
-     break
     }
-   }
-   if (identical(dropout_col, character(0))) {
-    dropout_col <- NA
-   }
-   return(dropout_col)
-  })
 
- output <- ifelse(output == "", NA, output)
+    data_subset <-
+      output <- apply(data, 1, function(row) {
+        dropout_col <- character(1)
+        for (i in 1:(ncol(data))) {
+          if (all(is.na(row[i:ncol(data)]))) {
+            dropout_col <- colnames(data)[i]
+            break
+          }
+        }
+        if (identical(dropout_col, character(0))) {
+          dropout_col <- NA
+        }
+        return(dropout_col)
+      })
 
- drop_data <- data.frame(dropout_column = output)
+    output <- ifelse(output == "", NA, output)
 
- if (!any(is.na(data[, ncol(data)]))) {
-  warning("No Dropouts detected. \n Please set last_col to the the last survey item. Use ?drop_detect to find out more")
- }
+    drop_data <- data.frame(dropout_column = output)
 
- drop_data$dropout <- !is.na(drop_data$dropout_column)
+    if (!any(is.na(data[, ncol(data)]))) {
+      warning("No Dropouts detected. \n Please set last_col to the the last survey item. Use ?drop_detect to find out more")
+    }
 
- # experimental (dropout index)
+    drop_data$dropout <- !is.na(drop_data$dropout_column)
 
- drop_data$dropout_index <- ifelse(!is.na(drop_data$dropout_column), match(drop_data$dropout_column, colnames(data)), NA)
+    # experimental (dropout index)
 
- if (requireNamespace("tibble", quietly = TRUE)) {
-  drop_data <- tryCatch(
-   as_tibble(drop_data),
-   error = function(e) {
-    drop_data$dropout <- as.logical(drop_data$dropout)
-    drop_data
-   }
-  )
- }
+    drop_data$dropout_index <- ifelse(!is.na(drop_data$dropout_column), match(drop_data$dropout_column, colnames(data)), NA)
 
- if (missing(last_col)){
-  return(list(list_data = drop_data, list_index = ncol(data)))
- } else {
-  return(list(list_data = drop_data, list_index = col_index))
- }
+    if (requireNamespace("tibble", quietly = TRUE)) {
+      drop_data <- tryCatch(
+        as_tibble(drop_data),
+        error = function(e) {
+          drop_data$dropout <- as.logical(drop_data$dropout)
+          drop_data
+        }
+      )
+    }
 
-}
+    if (missing(last_col)){
+      return(list(list_data = drop_data, list_index = ncol(data)))
+    } else {
+      return(list(list_data = drop_data, list_index = col_index))
+    }
+
+  }
