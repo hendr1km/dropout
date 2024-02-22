84572aa8d33d6d35430d4a43fec6db71cd8217b3
hendr1km <hendrikm@outlook.de>
2024-02-22 23:20:22 +0100

    removing start up message

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
- packageStartupMessage("This is version ", packageVersion(pkgname),
-                       " of ", pkgname,". \n Please report any issues at https://github.com/hendr1km/dropout/issues")
-}
